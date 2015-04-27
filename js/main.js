0fd56492e317bd0dc17ad248885cb5a933318c09
Pierre-Gildas MILLON <pierre-gildas.millon@ineat-conseil.fr>
2015-04-27 17:36:08 +0200

    feat(list): show announces count

--- js/main.js
+++ js/main.js
@@ -1,27 +1,14 @@
 jQuery(document).ready(function($) {
   var houseTemplate = $('#houseTemplate').html();
-  Mustache.parse(houseTemplate);
-  console.debug(houseTemplate);
+  var pagerTemplate = $('#pagerTemplate').html();
 
-  $(document).ajaxStart(function() {
-    $('#ajax-loader').show();
-  });
-
-  $(document).ajaxComplete(function() {
-    $('#ajax-loader').hide();
-  });
-
-  $('.lbc-menu .lbc-city').click(function() {
-    var city = $(this).text();
-    console.debug(city);
-
-    $('#lbc-content').empty();
-
+  function fetch(city, page) {
     $.getJSON('/api', {
       'location': city
     }, function(data) {
       console.debug(data);
 
+      $('#lbc-pager').html(Mustache.render(pagerTemplate, {count: data.houses.length}));
       if(data.houses && data.houses.length > 0) {
         $.each(data.houses, function(index, house) {
           var houseElem = $(Mustache.render(houseTemplate, house));
@@ -34,10 +21,29 @@ jQuery(document).ready(function($) {
             return false;
           });
         });
-      } else {
-        $('#lbc-content').append("Aucune annonce");
       }
     });
+  }
 
+  Mustache.parse(houseTemplate);
+  Mustache.parse(pagerTemplate);
+
+  $(document).ajaxStart(function() {
+    $('#ajax-loader').show();
+    $('#lbc-pager').html('Chargement en cours...');
+  });
+
+  $(document).ajaxComplete(function() {
+    $('#ajax-loader').hide();
+  });
+
+  $('.lbc-menu .lbc-city').click(function() {
+    var city = $(this).data('city');
+    console.debug(city);
+
+    $('#lbc-content').empty();
+    $('#lbc-pager').data('page', 1);
+
+    fetch(city, 1);
   })
 });
