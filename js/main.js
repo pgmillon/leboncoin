95b61248cfac86e9b85884c29d7a0623c26fa602
Pierre-Gildas MILLON <pierre-gildas.millon@ineat-conseil.fr>
2015-04-28 10:00:33 +0200

    feat(list): implement pagination

--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@ jQuery(document).ready(function($) {
   var pagerTemplate = $('#pagerTemplate').html();
 
   function fetch(city, page) {
+    $('#lbc-content').empty();
+
     $.getJSON('/api', {
-      'location': city
+      'location': city,
+      'page': page
     }, function(data) {
       console.debug(data);
 
-      $('#lbc-pager').html(Mustache.render(pagerTemplate, {count: data.houses.length}));
+      $('#lbc-pager').html(Mustache.render(pagerTemplate, {count: data.count?data.count:0}));
+
+      if(35 < data.count) {
+        $('#lbc-pager .next').show();
+      }
+
       if(data.houses && data.houses.length > 0) {
         $.each(data.houses, function(index, house) {
           var houseElem = $(Mustache.render(houseTemplate, house));
@@ -37,12 +45,19 @@ jQuery(document).ready(function($) {
     $('#ajax-loader').hide();
   });
 
+  $(document).on('click', '#lbc-pager .next', function() {
+    var city = $('#lbc-pager').data('city');
+    var page = $('#lbc-pager').data('page');
+
+    fetch(city, parseInt(page, 10) + 1);
+  });
+
   $('.lbc-menu .lbc-city').click(function() {
     var city = $(this).data('city');
     console.debug(city);
 
-    $('#lbc-content').empty();
     $('#lbc-pager').data('page', 1);
+    $('#lbc-pager').data('city', city);
 
     fetch(city, 1);
   })
